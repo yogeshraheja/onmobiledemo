d8e250440a10bad37d4443d4b8c19817a1529102
Yogesh Raheja <yogesh.raheja@thinknyx.com>
2022-08-18 10:43:30 +0000

    adding files

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Hello everyone we are learning Docker"
+
+@app.route('thinknyxjenkins')
+def hello():
+    return "We are trying to build this page using the power of Jenkins"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=8080)
